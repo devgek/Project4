0d46519cd5f9e0447fb397b785237f5ca22cb9b1
devgek <gerald.kahrer@gmail.com>
2015-04-02 14:34:54 +0200

    Version 1.0 nach Aufteilung

---
+++ PTimePro/src/com/gek/and/ptime/pro/PTimeProApp.java
@@ -0,0 +1,13 @@
+package com.gek.and.ptime.pro;
+
+import com.gek.and.geklib.type.AppType;
+import com.gek.and.project4.app.Project4App;
+
+public class PTimeProApp extends Project4App{
+
+	@Override
+	protected void setAppType() {
+		this.appType = AppType.PRO;
+	}
+
+}
