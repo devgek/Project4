8f87eace3fcbae19bf5072bfe6c16a19f96dce2e
devgek <gerald.kahrer@gmail.com>
2015-04-02 14:20:05 +0200

    Version 1.0 fertig

---
+++ PTime/src/com/gek/and/ptime/free/PTimeApp.java
@@ -0,0 +1,13 @@
+package com.gek.and.ptime.free;
+
+import com.gek.and.geklib.type.AppType;
+import com.gek.and.project4.app.Project4App;
+
+public class PTimeApp extends Project4App{
+
+	@Override
+	protected void setAppType() {
+		this.appType = AppType.FREE;
+	}
+ 
+}
